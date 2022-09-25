203883c7fc425d4f291a409a09f9eac4c74a2094
Niswin.s <niswins03@gmail.com>
2022-09-25 22:54:39 +0530

    my app

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView textView;
+    private Ringtone ringtone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView);
+        ringtone = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+        BroadcastReceiver broadcastReceiverBattrery = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Integer integerBatteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+                textView.setText(integerBatteryLevel.toString());
+                if (integerBatteryLevel > 99){
+                    ringtone.play();
+                }
+            }
+        };
+        registerReceiver(broadcastReceiverBattrery, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    public void stopButton(View view){
+        ringtone.stop();
+    }
+}
+
